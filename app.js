0c2cd7118074010323877791e476d83b25375e38
pedro-ds <37834497+pedro-ds@users.noreply.github.com>
2023-04-23 19:03:00 -0300

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fetch             = require('node-fetch');
 
 const app               = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
